a77d0afc49954384a903a3a2477b914438426140
Zhuyi Xue <alfred532008@gmail.com>
2018-06-13 14:53:23 -0700

    added check_tag_multiplicity.py

---
+++ check_tag_multiplicity.py
@@ -0,0 +1,29 @@
+import sys
+from collections import Counter
+
+"""
+Check what tags in the attribute column could appear multiple times per entry
+"""
+
+
+from gtf2csv import read_gtf, parse_attr
+
+
+gtf_filename = sys.argv[1]
+
+df = read_gtf(gtf_filename)
+
+multi_tags = set()
+for k, row in df.iterrows():
+    attrs_str = row.attribute
+    attrs = attrs_str.strip(';').split(';')
+    tags = []
+    for attr in attrs:
+        tag, value = parse_attr(attr)
+        tags.append(tag)
+    count_dd = Counter(tags)
+    for t, n in count_dd.items():
+        if n > 1:
+            multi_tags.add(t)
+    if (k + 1) % 10000 == 0:
+        print('processed {0} rows'.format(k + 1))
