1b11318772af92353755ced6632f432126d36d75
Zhuyi Xue <alfred532008@gmail.com>
2018-09-04 15:30:48 -0700

    parallelized gtf2csv.py and handle unknown multiplicity_tags correctly

--- gtf2csv.py
+++ gtf2csv.py
@@ -1,6 +1,9 @@
 import logging
+import multiprocessing
+from collections import Counter
 import argparse
 
+from tqdm import tqdm
 import pandas as pd
 
 
@@ -21,22 +24,35 @@ def parse_attr(attr):
     return (tag, value.strip('"'))
 
 
-def parse_attrs_str(attrs_str):
+def parse_attrs_str(attrs_str, multiplicity_tags):
+    """
+    :params multiplicity_tags: a set of tags potentially appearing multiple
+    times in one GTF entry
+    """
     # strip: remove last ;
     attrs = attrs_str.strip(';').split(';')
     res = {}
     for attr in attrs:
         tag, value = parse_attr(attr)
-        # since there could be multiple tags per row,
-        # e.g. `... tag "cds_end_NF"; tag "mRNA_end_NF";`
-
-        # I verified that only tag is the tag that may appear multiple times
-        # per row, so I decided to use the binary (1/0) columns based on the
-        # value of tag
-        if tag == "tag":
-            res[value] = 1
+        # convert multiplicity_tag into a binary column
+        if tag in multiplicity_tags:
+            res[f'{tag}_{value}'] = 1
         else:
             res[tag] = value
+    return res
+
+
+def parse_attribute_column(attribute_list, multiplicity_tags, num_cpus):
+    """
+    :params attribute_series: a list of values for the GTF attribute column
+    """
+    params = []
+    # TODO: parallelized but could be memory intensive
+    for attr_str in attribute_list:
+        params.append((attr_str, multiplicity_tags))
+
+    with multiprocessing.Pool(num_cpus) as p:
+        res = p.starmap(parse_attrs_str, params)
     return res
 
 
@@ -61,6 +77,10 @@ def get_args():
         help=('the output filename, if not specified, would just set it to be '
               'the same as the input but with extension replaced (gtf => csv)')
     )
+    parser.add_argument(
+        '-t', '--num-cpus', type=int,
+        help='number of cpus for parallel processing, default to all cpus available'
+    )
     return parser.parse_args()
 
 
@@ -74,17 +94,51 @@ def gen_output(input_gtf):
     return output_csv
 
 
-def main(filename):
+def check_multiplicity_per(row):
+    res = []
+    attrs_str = row.attribute
+    attrs = attrs_str.strip(';').split(';')
+    tags = []
+    for attr in attrs:
+        tag, value = parse_attr(attr)
+        tags.append(tag)
+    count_dd = Counter(tags)
+    for t, n in count_dd.items():
+        if n > 1:
+            res.append(t)
+    return res
+
+
+def get_multiplicity_tags(df_gtf, num_cpus):
+    """
+    check which tags could appear multiple values in the attribute column of
+    one gtf entry (e.g. ont, tag)
+    """
+    params = []
+    # TODO: parallelized but could be memory intensive
+    for k, row in tqdm(df_gtf.iterrows()):
+        params.append(row)
+
+    with multiprocessing.Pool(num_cpus) as p:
+        res = p.map(check_multiplicity_per, params)
+
+    return set(i for j in res for i in j)
+
+
+def main(filename, num_cpus):
     """
     1. read gtf
     2. extract attributes as separate columns
     3. transform to a pandas dataframe
     """
     df = read_gtf(filename)
+    mlp_tags = get_multiplicity_tags(df, num_cpus)
+
+    parsed = parse_attribute_column(df.attribute.values, mlp_tags, num_cpus)
 
     logging.info('converting to dataframe...'.format(filename))
-    attr_df = pd.DataFrame.from_dict(
-        df.attribute.apply(parse_attrs_str).values.tolist())
+    attr_df = pd.DataFrame.from_dict(parsed)
+
     df.drop('attribute', axis=1, inplace=True)
     ndf = pd.concat([df, attr_df], axis=1)
     return ndf
@@ -93,12 +147,13 @@ def main(filename):
 if __name__ == "__main__":
     args = get_args()
     gtf_path = args.gtf
+    num_cpus = args.num_cpus if args.num_cpus else multiprocessing.cpu_count()
 
     output_csv = args.output
     if output_csv is None:
         output_csv = gen_output(gtf_path)
 
-    ndf = main(gtf_path)
+    ndf = main(gtf_path, num_cpus)
 
     # sort attribute columns without reordering GTF_COLS
     cols = ndf.columns.tolist()
